3cf00b8dca046f96de1588f6c062239979121286
Furkan Hanci <60580341+Furkanus@users.noreply.github.com>
2020-07-25 18:10:30 +0300

    Add files via upload

---
+++ SwiftUIFurkan/SwiftUIFurkan/SwiftUIFurkanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFurkanApp.swift
+//  SwiftUIFurkan
+//
+//  Created by Furkan Hanci on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFurkanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
